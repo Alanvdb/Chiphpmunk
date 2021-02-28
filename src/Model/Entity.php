52d54686c74e4cb95a10fdd78cc977e088a1255c
AlanVdb <alanvdb.coder@gmail.com>
2021-02-28 20:33:47 +0100

    Improved Entity class.

--- src/Model/Entity.php
+++ src/Model/Entity.php
@@ -2,12 +2,24 @@
 
 namespace Chiphpmunk\Model;
 
+use Chiphpmunk\Functionality\HydratorTrait;
+
 abstract class Entity
 {
+    use HydratorTrait;
+
     /**
      * @var mixed id Entity identifier
      */
     private $id;
+
+    /**
+     * Constructor
+     */
+    public function __construct(array $data = [])
+    {
+        $this->hydrate($data);
+    }
 
     /**
      * @return mixed Entity identifier
@@ -28,5 +40,44 @@ abstract class Entity
     {
         $this->id = $id;
         return $this;
+    }
+
+    /**
+     * @return bool whether or not entity has an id
+     */
+    public function hasId() : bool
+    {
+        return $this->id !== null;
+    }
+
+    /**
+     * Magic getter
+     *
+     * @param string $name Attribute name
+     * 
+     * @return mixed
+     */
+    public function __get(string $name)
+    {
+        $method = 'get' . ucfirst($name);
+        if (method_exists($this, $method)) {
+            return $this->$method();
+        }
+    }
+
+    /**
+     * Magic setter
+     *
+     * @param string $name  Attribute name
+     * @param mixed  $value Attribute value
+     * 
+     * @return mixed
+     */
+    public function __set(string $name, $value)
+    {
+        $method = 'set' . ucfirst($name);
+        if (method_exists($this, $method)) {
+            return $this->$method($value);
+        }
     }
 }
